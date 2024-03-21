c41e7cd0db7813411eaeeb881f93e263242ed67a
umerishaq123 <umerishaq7387@gmail.com>
2024-03-21 16:54:34 +0500

    login_signup

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,11 @@
+import Signup from "../features/Signup";
+
+function SignupPage() {
+    return ( 
+        <div>
+            <Signup></Signup>
+        </div>
+     );
+}
+
+export default SignupPage;
